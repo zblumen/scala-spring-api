00b5e776b24fbcd9486dd8c1062a31bd88095e02
Zachary Blumenfeld <zblumenf@gmail.com>
2019-12-25 10:37:54 -0500

    Init

---
+++ src/main/scala/scalaDemo/Application.scala
@@ -0,0 +1,10 @@
+package scalaDemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+object Application extends App {
+  SpringApplication.run(classOf[Application]);
+}
